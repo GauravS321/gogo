81e0852338f77d60c94ca05611acd6f6844cca5a
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-23 16:49:46 +0530

    Update verify.js

--- src/web/controllers/plugins/dave/academic/verify.js
+++ src/web/controllers/plugins/dave/academic/verify.js
@@ -1,19 +1,26 @@
 const { retrieve } = require('../../../../../../functions/plugins/dave/academic');
 
 module.exports.get = async (req, res) => {
-    try {
-        let response = await retrieve(req.query);
+        try {
+                let response = await retrieve(req.query);
+                if(req.user && req.isAuthenticated()) {
 
-        return res.render('plugins/dave/academic/verification', {
-            data: response.msg['data'],
-            username: req.user.username,
-            email: req.user.email
-        });
-    } catch (error) {
-        return res.render('plugins/dave/academic/verification', {
-            error_msg: "Whoops, something went wrong, Please try later..",
-            username: req.user.username,
-            email: req.user.email
-        });
-    }
+                    return res.render('plugins/dave/academic/verification', {
+                        data: response.msg['data'],
+                        username: req.user.username,
+                        email: req.user.email
+                    });
+                }
+                else {
+                return res.render('plugins/dave/academic/verification', {
+                                data: response.msg['data']
+                            });
+                 }
+        } catch (error) {
+            return res.render('plugins/dave/academic/verification', {
+                error_msg: "Whoops, something went wrong, Please try later..",
+                username: req.user.username,
+                email: req.user.email
+            });
+        }   
 }
