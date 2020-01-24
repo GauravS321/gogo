a7fc174f6fb3ed5cd64cdfef4e7a33a5aee2f476
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-24 15:22:05 +0530

    Update verify.js

--- src/web/controllers/plugins/dave/academic/verify.js
+++ src/web/controllers/plugins/dave/academic/verify.js
@@ -4,8 +4,6 @@ module.exports.get = async (req, res) => {
     try {
         let response = await retrieve(req.query);
 
-        console.log(response);
-
         return res.render('plugins/dave/academic/verification', {
             data: response.msg['data'],
             username: (req.user) ? req.user.username : false,
@@ -13,9 +11,9 @@ module.exports.get = async (req, res) => {
         });
     } catch (error) {
         return res.render('plugins/dave/academic/verification', {
-            error_msg: "Whoops, something went wrong, Please try later..",
-            username: req.user.username,
-            email: req.user.email
+            error_msg: error.error,
+            username: (req.user) ? req.user.username : false,
+            email: (req.user) ? req.user.email : false
         });
     }
 }
