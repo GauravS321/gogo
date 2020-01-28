3772dd68f7c15864e3f4bed2a39a29d09f15db5c
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-28 14:14:01 +0530

    Update send.js

--- src/web/controllers/users/account/myassets/send.js
+++ src/web/controllers/users/account/myassets/send.js
@@ -1,4 +1,4 @@
-const send = require('../../../../../../functions/components/sam/send')
+const send = require('../../../../../../functions/users/account/myassets/send')
 module.exports.get = (req, res) => {
     if (req.user && req.isAuthenticated()) {
         return res.render('users/account/send',
@@ -19,21 +19,21 @@ module.exports.post = async (req, res) => {
             let response = await send(req.user.primechain_address, receiver_address, asset_name, quantity, asset_description);
 
             if (response.status === 200) {
-                req.flash('success_msg', "Asset successfully sent!!, TX ID" + response.msg)
-                return res.redirect('/users/account/myassets/transfer')
+                req.flash('success_msg', "Asset successfully transferred!!, TX ID" + response.msg)
+                return res.redirect('/account/myassets/transfer')
             }
         } catch (error) {
             if (error.errors) {
                 req.flash('errors', error.errors);
-                return res.redirect('/users/account/myassets/transfer');
+                return res.redirect('/account/myassets/transfer');
             }
             else if (error.error) {
                 req.flash('error_msg', error.error);
-                return res.redirect('/users/account/myassets/transfer');
+                return res.redirect('/account/myassets/transfer');
             }
             else {
                 req.flash('error_msg', "Internal server error!!!");
-                return res.redirect('/users/account/myassets/transfer');
+                return res.redirect('/account/myassets/transfer');
             }
         }
     }
