ccbcfeb571d74e10f22882dcb2f1b58b53f76c5d
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-28 13:30:52 +0530

    Edits

--- src/web/routes/users/index.js
+++ src/web/routes/users/index.js
@@ -58,7 +58,7 @@ router.post('/account/change-password', changePasswordContoller.post);
 
 // My assets - transfer asset
 router.get('/account/myassets/transfer', myAssetsTransferController.get);
-router.get('/account/myassets/:name', myAssetsSendController.get);
+router.get('/account/myassets/transfer/:name', myAssetsSendController.get);
 router.post('/account/myassets/send', myAssetsSendController.post);
 
 // User logout
