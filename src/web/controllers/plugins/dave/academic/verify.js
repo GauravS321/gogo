ce9cad83022318047a83bf02d0a88ac64c8e87a4
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-29 15:32:31 +0530

    Update verify.js

--- src/web/controllers/plugins/dave/academic/verify.js
+++ src/web/controllers/plugins/dave/academic/verify.js
@@ -17,3 +17,20 @@ module.exports.get = async (req, res) => {
         });
     }
 }
+module.exports.getQRCode = async (req, res) => {
+    try {
+        const { text, txid_signature, password, iv, trade_channel_name } = req.query;
+
+        return res.render('plugins/dave/academic/qrcode', {
+            data: `${text}&txid_signature=${txid_signature}&password=${password}&iv=${iv}&trade_channel_name=${trade_channel_name}`,
+            username: (req.user) ? req.user.username : false,
+            email: (req.user) ? req.user.email : false
+        });
+    } catch (error) {
+        return res.render('plugins/dave/academic/verification', {
+            error_msg: error.error,
+            username: (req.user) ? req.user.username : false,
+            email: (req.user) ? req.user.email : false
+        });
+    }
+}
