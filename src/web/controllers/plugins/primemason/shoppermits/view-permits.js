2fc8ad7a8eb978eb76641039d3f787835c41cbc8
Anil Nakoti <anil@primechaintech.com>
2020-01-30 18:53:21 +0530

    fixed view-permit

--- src/web/controllers/plugins/primemason/shoppermits/view-permits.js
+++ src/web/controllers/plugins/primemason/shoppermits/view-permits.js
@@ -1,22 +1,23 @@
 const shoppermits = require('../../../../models/plugins/primemason/shoppermits');
 
 module.exports.get = async (req, res) => {
-        try {
-            const uuid = req.query.uuid;
-            
-            const record = await shoppermits.findOne({uuid});
+    try {
+        const uuid = req.query.uuid;
 
-            return res.render('plugins/primemason/shoppermits/view-permits', {
-                shopname: record.json['Shop Name'],
-                ownername: record.json['Owner Name'],
-                dataArr: record.permits,
-                username: req.user.username,
-                email: req.user.email
-            });
-        } catch (error) {
-            return res.render('plugins/primemason/shoppermits/view-permits', {
-                username: req.user.username,
-                email: req.user.email
-            });
-        }
+        const record = await shoppermits.findOne({ uuid });
+
+        return res.render('plugins/primemason/shoppermits/view-permits', {
+            shopname: record.json['Shop Name'],
+            ownername: record.json['Owner Name'],
+            dataArr: record.permits,
+            username: (req.user) ? req.user.username : false,
+            email: (req.user) ? req.user.email : false
+        });
+    } catch (error) {
+        return res.render('plugins/primemason/shoppermits/view-permits', {
+            error_msg: error.error,
+            username: (req.user) ? req.user.username : false,
+            email: (req.user) ? req.user.email : false
+        });
+    }
 }
