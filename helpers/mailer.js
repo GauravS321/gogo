7bae241782459f6dbe2ee0dea3c23e33b3733581
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-30 18:36:04 +0530

    Update mailer.js

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -470,11 +470,11 @@ class MailNotificationEngine {
                             </strong></p>
                             <p>A document has been shared with you.</p>
 
-                        <a href='${this.baseUrl}/plugins/primemason/${use_case}/verification?${path}' style='padding: 8px 20px; background-color: #4B72FA; color: #fff; font-weight: bolder; font-size: 16px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;'>View document
+                        <a href='${this.baseUrl}/plugins/primemason/${use_case}/view-permits?${path}' style='padding: 8px 20px; background-color: #4B72FA; color: #fff; font-weight: bolder; font-size: 16px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;'>View document
                         </a>
                         <br>
                         
-                        <a href="${this.baseUrl}/plugins/primemason/${use_case}/qrcode?text=${this.baseUrl}/plugins/primemason/${use_case}/verification?${path} style='padding: 8px 20px; background-color: #4B72FA; color: #fff; font-weight: bolder; font-size: 16px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;'">View QRcode
+                        <a href="${this.baseUrl}/plugins/primemason/${use_case}/qrcode?text=${this.baseUrl}/plugins/primemason/${use_case}/view-permits?${path} style='padding: 8px 20px; background-color: #4B72FA; color: #fff; font-weight: bolder; font-size: 16px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;'">View QRcode
                         </a>
                         <br>
                         <br>`;
