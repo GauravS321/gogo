b84ce94ab5c4309046393984466bf9a2fe866d8b
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-30 18:31:55 +0530

    Update share.js

--- src/web/controllers/plugins/primemason/shoppermits/share.js
+++ src/web/controllers/plugins/primemason/shoppermits/share.js
@@ -5,15 +5,13 @@ module.exports.post = async (req, res) => {
         try {
             let use_case = req.baseUrl.split('/')[3];
             const {receiver_name, receiver_email, uuid} = req.body;
-            
             await share(req.user.email, receiver_name, receiver_email, uuid, use_case);
 
             req.flash("success_msg", "Permits detail e-mail sent successfully!!!")
-            return res.redirect('/plugins/primemason/shoppermits/manage-permits');
-          
+            return res.redirect('/plugins/primemason/shoppermits/manage-shops');
         } catch (error) {
             req.flash("error_msg", error.message);
-            return res.redirect('/plugins/primemason/shoppermits/manage-permits');
+            return res.redirect('/plugins/primemason/shoppermits/manage-shops');
         }
     }
     return res.redirect('/login');
