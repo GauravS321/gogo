b6da284b83d886af545fcc33ba20e03f80845afc
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-30 14:16:12 +0530

    PrimeMason

--- src/web/controllers/plugins/primemason/shoppermits/create.js
+++ src/web/controllers/plugins/primemason/shoppermits/create.js
@@ -12,6 +12,7 @@ module.exports.get = (req, res) => {
 
 module.exports.post = async (req, res) => {
     try {
+        console.log(req.body)
         if (req.user && req.isAuthenticated()) {
             let response = await create(req.body);
 
