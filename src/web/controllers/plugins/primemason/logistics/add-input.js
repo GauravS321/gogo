778d1397ea4239ea569567967fdcfbb8f1e38749
Anil Nakoti <anil@primechaintech.com>
2020-02-05 11:11:35 +0530

    fixed view of logistics

--- src/web/controllers/plugins/primemason/logistics/add-input.js
+++ src/web/controllers/plugins/primemason/logistics/add-input.js
@@ -2,15 +2,16 @@ const logistics = require('../../../../models/plugins/primemason/logistics');
 
 module.exports.post = async (req, res) => {
     try {
-        if (req.user && req.isAuthenticated()) {
-            let uuid = req.body.uuid;
-            let json = req.body;
-            delete json['uuid'];
-            await logistics.findOneAndUpdate({uuid},{$push: {inputs:json}});
+        let uuid = req.body.uuid;
+        let json = req.body;
+        delete json['uuid']
 
-            req.flash("success_msg", "Input added. ", uuid);
-            return res.redirect('/plugins/primemason/logistics/manage');
-        }
+
+        await logistics.findOneAndUpdate({ uuid }, { $push: { inputs: json } });
+
+        req.flash("success_msg", "Input added. ", uuid);
+        return res.redirect('/plugins/primemason/logistics/manage');
+
     } catch (error) {
         req.flash('error_msg', "Oops. Something went wrong.");
         return res.redirect('/plugins/primemason/logistics/manage');
