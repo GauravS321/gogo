43143d24c7cde344bd085e37064af640d91fb914
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-13 17:19:21 +0530

    Update index.js

--- src/web/routes/users/index.js
+++ src/web/routes/users/index.js
@@ -28,7 +28,7 @@ const myAssetsSendController = require('../../controllers/users/account/myassets
 // My assets create offer controller
 const myAssetsCreateOfferController = require('../../controllers/users/account/myassets/offer');
 const myAssetsListMyPublicOffersController = require('../../controllers/users/account/myassets/list');
-const myAssetsListAllPublicOffersController = require('../../controllers/users/account/myassets/list-all');
+//const myAssetsListAllPublicOffersController = require('../../controllers/users/account/myassets/list-all');
 
 // User change-password controller
 const changePasswordContoller = require('../../controllers/users/account/change-password');
@@ -73,7 +73,7 @@ router.post('/account/myassets/send', myAssetsSendController.post);
 router.get('/account/myassets/offer/create/:name', myAssetsCreateOfferController.get);
 router.post('/account/myassets/offer/create', myAssetsCreateOfferController.post);
 router.get('/account/myassets/offer/list', myAssetsListMyPublicOffersController.get);
-router.get('/account/myassets/offer/list-all', myAssetsListAllPublicOffersController.get);
+//router.get('/account/myassets/offer/list-all', myAssetsListAllPublicOffersController.get);
 
 // User logout
 router.get('/account/logout', logoutContoller.get);
