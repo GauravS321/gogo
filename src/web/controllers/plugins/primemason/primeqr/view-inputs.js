d6f74d99b8e9fd4a73890f3cd0ada9451f253ead
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-19 16:58:57 +0530

    Update view-inputs.js

--- src/web/controllers/plugins/primemason/primeqr/view-inputs.js
+++ src/web/controllers/plugins/primemason/primeqr/view-inputs.js
@@ -1,4 +1,5 @@
 const primeqr = require('../../../../models/plugins/primemason/primeqr');
+const moment = require('moment');
 
 module.exports.get = async (req, res) => {
     try {
@@ -6,10 +7,17 @@ module.exports.get = async (req, res) => {
 
         const record = await primeqr.findOne({ uuid });
         let image = record.json['image'];
+        let best_before = moment().isBefore(record.json['Best before date']);
+        console.log(best_before);
+
+        let expired = moment().isBefore(record.json['Expiry date']);
+        console.log(expired);
 
         delete record.json['image'];
 
         return res.render('plugins/primemason/primeqr/view-inputs', {
+            best_before,
+            expired,
             image,
             uuid,
             dataArr: record.json,
