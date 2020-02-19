3b8baf2b762636ae93598d14a69bc38db463ec62
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-19 17:17:21 +0530

    a

--- src/web/controllers/plugins/primemason/primeqr/create.js
+++ src/web/controllers/plugins/primemason/primeqr/create.js
@@ -35,12 +35,9 @@ module.exports.post = (req, res) => {
                         message: err
                     });
                 }
-
-                console.log(req.body);
-                console.log(req.files);
                 
                 let json = req.body;
-                json['image'] = (req.files.primechainImages)? req.files[0].path: "";
+                json['image'] = (req.files)? req.files[0].path: "";
                 let response = await create(req.body);
 
                 req.flash("success_msg", "PrimeQR created. ", response.msg['uuid']);
