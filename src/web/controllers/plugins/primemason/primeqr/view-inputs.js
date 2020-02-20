dd2d97f6c828557819bcf86e65b69de999983a96
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-20 11:18:41 +0530

    Update view-inputs.js

--- src/web/controllers/plugins/primemason/primeqr/view-inputs.js
+++ src/web/controllers/plugins/primemason/primeqr/view-inputs.js
@@ -8,8 +8,8 @@ module.exports.get = async (req, res) => {
         const record = await primeqr.findOne({ uuid });
         let image = record.json['image'];
         record.json['manufactured_display'] = moment(record.json['Manufacturing date']).format('ll');
-        record.json['best_before_display'] = moment(record.json['Best before date']).format();
-        record.json['expiry_display'] = moment(record.json['Expiry date'], "YYYY-MM-DD" ,"DD-MMM-YYYY");
+        record.json['best_before_display'] = moment(record.json['Best before date']).format('ll');
+        record.json['expiry_display'] = moment(record.json['Expiry date']).format('ll');
 
         let best_before = moment().isAfter(record.json['Best before date']);
         let expired = moment().isAfter(record.json['Expiry date']);
@@ -17,9 +17,6 @@ module.exports.get = async (req, res) => {
         delete record.json['Manufacturing date'];
         delete record.json['Best before date'];
         delete record.json['Expiry date'];
-
-                console.log(record.json);
-
 
         return res.render('plugins/primemason/primeqr/view-inputs', {
             best_before,
