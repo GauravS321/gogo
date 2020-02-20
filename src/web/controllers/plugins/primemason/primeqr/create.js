c482c554b9041799c2ec2a33c4e1c70c4a690c11
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-20 13:43:53 +0530

    a

--- src/web/controllers/plugins/primemason/primeqr/create.js
+++ src/web/controllers/plugins/primemason/primeqr/create.js
@@ -35,6 +35,8 @@ module.exports.post = (req, res) => {
                         message: err
                     });
                 }
+                console.log(req.body);
+                console.log(req.files);
                 
                 let json = req.body;
                 json['image'] = (req.files)? req.files[0].path: "";
