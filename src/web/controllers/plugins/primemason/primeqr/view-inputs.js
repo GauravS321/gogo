2e8c2f4c1746c1a96a81236bec4769793e37d097
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-20 17:51:48 +0530

    a

--- src/web/controllers/plugins/primemason/primeqr/view-inputs.js
+++ src/web/controllers/plugins/primemason/primeqr/view-inputs.js
@@ -14,14 +14,14 @@ module.exports.get = async (req, res) => {
         let best_before = moment().isAfter(record.json['Best before date']);
         let expired = moment().isAfter(record.json['Expiry date']);
 
-        let comment_image = records.inputs['comment_image'];
-        delete records.inputs['comment_image'];
-        let latitude = (records.inputs.geo_latitude.length > 0) ? true: delete records.inputs.geo_latitude;
-        let longtitude = (records.inputs.geo_longitude.length > 0) ? true: delete records.inputs.geo_longitude;
+        let comment_image = record.inputs['comment_image'];
+        delete record.inputs['comment_image'];
+        let latitude = (record.inputs.['geo_latitude']) ? true: delete record.inputs['geo_latitude'];
+        let longtitude = (record.inputs['geo_longitude']) ? true: delete record.inputs['geo_longitude'];
 
         // delete record.json['Best before date'];
         // delete record.json['Expiry date'];
-
+        console.log(record.json)
         return res.render('plugins/primemason/primeqr/view-inputs', {
             best_before,
             expired,
