13e860cd60539d307c34d3c07c86f1dcf9f47d1c
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-24 14:44:08 +0530

    Update mailer.js

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -19,16 +19,18 @@ class MailNotificationEngine {
         };
 
         this.emailTop = `<html>
-                            <body style="background-color: #222533; padding: 20px; font-size: 14px; line-height: 1.43; font-family:Helvetica Neue Segoe UI, Helvetica, Arial, sans-serif;">
-                                <div style="max-width: 600px; margin: 0px auto; background-color: #fff; box-shadow: 0px 20px 50px rgba(0,0,0,0.05);">
-                                    <table style="width: 100%;">
-                                        <tr>
-                                            <td style="background-color: #fff;">
-                                                <img alt="" src="https://www.primechaintech.com/images/logo-2.png" padding: 20px">
-                                            </td>
-                                        </tr>
-                                    </table>
-                                    <div style="padding: 20px 70px; border-top: 1px solid rgba(0,0,0,0.05);">`;
+  <body style="background-color: #222533; padding: 20px; font-family: font-size: 14px; line-height: 1.43; font-family: &quot;Helvetica Neue&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif;">
+    <div style="max-width: 600px; margin: 0px auto; background-color: #fff; box-shadow: 0px 20px 50px rgba(0,0,0,0.05);">
+      <table style="width: 100%;">
+        <tr>
+          <td style="background-color: #fff;">
+            <img alt="" src="https://www.primechaintech.com/images/primechain_email.png" style="width: 70px; padding: 20px">
+          </td>
+          <td style="padding-left: 50px; text-align: right; padding-right: 20px;">
+            <a href="https://www.primechaintech.com/" style="color: #261D1D; text-decoration: underline; font-size: 14px; letter-spacing: 1px;">Need help?</a>
+          </td>
+        </tr>
+      </table><img alt="" src="https://www.primechaintech.com/images/primechain_email_image.jpg" style="max-width: 100%; height: auto;">`;
 
         this.emailBottom = `        Have an amazing day!<br>Team Primechain<br/><br/>
 
