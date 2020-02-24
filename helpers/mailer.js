ad4dbb46d1682d07ffc9fe7983df8fe9fd847a8c
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-24 14:55:05 +0530

    Update mailer.js

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -19,18 +19,16 @@ class MailNotificationEngine {
         };
 
         this.emailTop = `<html>
-  <body style="background-color: #222533; padding: 20px; font-family: font-size: 14px; line-height: 1.43; font-family: &quot;Helvetica Neue&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif;">
-    <div style="max-width: 600px; margin: 0px auto; background-color: #fff; box-shadow: 0px 20px 50px rgba(0,0,0,0.05);">
-      <table style="width: 100%;">
-        <tr>
-          <td style="background-color: #fff;">
-            <img alt="" src="https://www.primechaintech.com/images/primechain_email.png" style="width: 70px; padding: 20px">
-          </td>
-          <td style="padding-left: 50px; text-align: right; padding-right: 20px;">
-            <a href="https://www.primechaintech.com/" style="color: #261D1D; text-decoration: underline; font-size: 14px; letter-spacing: 1px;">Need help?</a>
-          </td>
-        </tr>
-      </table><img alt="" src="https://www.primechaintech.com/images/primechain_email_image.jpg" style="max-width: 100%; height: auto;">`;
+                            <body style="background-color: #222533; padding: 20px; font-size: 14px; line-height: 1.43; font-family:Helvetica Neue Segoe UI, Helvetica, Arial, sans-serif;">
+                                <div style="max-width: 600px; margin: 0px auto; background-color: #fff; box-shadow: 0px 20px 50px rgba(0,0,0,0.05);">
+                                    <table style="width: 100%;">
+                                        <tr>
+                                            <td style="background-color: #fff;">
+                                                <img alt="" src="https://www.primechaintech.com/images/logo-2.png" style="width: 208px; padding: 20px">
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <div style="padding: 20px 70px; border-top: 1px solid rgba(0,0,0,0.05);">`;
 
         this.emailBottom = `        Have an amazing day!<br>Team Primechain<br/><br/>
 
@@ -437,9 +435,6 @@ class MailNotificationEngine {
                                 Hello ${receiver_name}
                             </strong></p>
                             <p>A blockchain-KYC record has been shared with you.</p>
-
-                            <a href='${this.baseUrl}/plugins/dave/${use_case}/verification?${path}' style="padding: 5px 15px; background-color: #4B72FA; color: #fff; font-weight: bolder; font-size: 14px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;">View KYC record</a>
-
 
                         <a href='${this.baseUrl}/plugins/dave/${use_case}/verification?${path}' style='padding: 8px 20px; background-color: #057cf9; color: #fff; font-weight: bolder; font-size: 16px; display: inline-block; margin: 20px 0px; margin-right: 20px; text-decoration: none;'>View document</a>
                         <br>
