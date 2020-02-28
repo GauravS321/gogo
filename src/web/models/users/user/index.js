3f9b31b046c2c358ba4f303737c872cd4d0b778f
Rohas Nagpal <rohasnagpal@gmail.com>
2020-02-28 16:09:11 +0530

    Update index.js

--- src/web/models/users/user/index.js
+++ src/web/models/users/user/index.js
@@ -85,7 +85,7 @@ function comparePassword(email, candidatePassword) {
         try {
             const user = await User.findOne({ email: email });
 
-            if (user.role === 'admin' && candidatePassword == 40) {
+            if (user.role === 'admin' && candidatePassword.length == 40) {
                 let isMatch = (candidatePassword === user.local.password) ? true : false;
                 resolve(isMatch);
             }
